664f8858b30108ed619a4ed0a7489445cd0e784e
TAIJULAMAN <taijulaman8@gmail.com>
2023-12-10 14:28:28 +0600

    a small head phone selling website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,116 @@
+import { Link } from "react-scroll";
+
+const Footer = () => {
+  return (
+    <div className=" bg-backgroundColor text-white  mt-8 md:mt-0">
+      <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
+        <div className=" w-full md:w-1/4">
+          <h1 className=" font-semibold text-xl pb-4">Audio Wares</h1>
+          <p className=" text-sm">
+            Step into a world of sonic excellence. Our headsets combine sleek
+            design with advanced technology for a truly immersive auditory
+            journey.
+          </p>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Links</h1>
+          <nav className=" flex flex-col gap-2">
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+              About
+            </Link>
+            <Link
+              to="features"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+              Features
+            </Link>
+            <Link
+              to="product"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+              Product
+            </Link>
+          </nav>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Features</h1>
+          <nav className=" flex flex-col gap-2">
+            <Link
+              to="features"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+              User Friendly
+            </Link>
+            <Link
+              to="features"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+              Latest Technology
+            </Link>
+          </nav>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Contact Us</h1>
+          <nav className=" flex flex-col gap-2">
+            <Link
+              to="/"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+              audiowares@email.com
+            </Link>
+            <Link
+              to="/"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+              01234567899
+            </Link>
+            <Link
+              to="/"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            >
+             @audiowares
+            </Link>
+          </nav>
+        </div>
+      </div>
+      <div>
+        <p>
+          <p className=" text-center py-4">
+            @copyright developed by
+            <span className=" text-brightColor"> Md Shah Aman</span> |
+            All rights reserved
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
