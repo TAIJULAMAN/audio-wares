f3960e70f6ec4282f417d175ebb91e27408b5d13
TAIJULAMAN <taijulaman8@gmail.com>
2024-04-23 04:08:40 +0600

    changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className=" bg-backgroundColor text-white  mt-8 md:mt-0">
       <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
         <div className=" w-full md:w-1/4">
-          <h1 className=" font-semibold text-xl pb-4">Audio Wares</h1>
+          <h1 className=" font-semibold text-xl pb-4">Beat Store</h1>
           <p className=" text-sm">
             Step into a world of sonic excellence. Our headsets combine sleek
             design with advanced technology for a truly immersive auditory
